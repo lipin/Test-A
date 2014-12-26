14479b543809717827836f80b0bf5c2040769230
lipin <ping.li1688@gmail.com>
2014-12-26 01:43:22 +0000

    sunny-数据库

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,9 +5,23 @@ class WelcomeController < ApplicationController
 	    	logger.info '**********************'
 	        logger.info body.inspect
 	        logger.info '----------------------'
-	    if body['message'] 
-	      render :text => "#{body['message']}"
+	    if body['sessionId']
+	      authentication = User.find_or_initialize_by session_id: body['sessionId']
+	      authentication.play_id="ping.li1688@gmail.com"
+	      authentication.attributes = ({
+	        session_id:  body['sessionId'],
+	        total_word_count: body['data']['numberOfWordsToGuess']
+	      })
+	      authentication.save
+	      return authentication
+	    else
+	      # logger.info body
+	      raise body
 	    end
+ 	 end
+ 	 def new 
+ 	 	authentication =User.last
+ 	 	
  	 end
  	
 end
